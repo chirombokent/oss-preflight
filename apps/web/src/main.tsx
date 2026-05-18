a76fed42496fc4b95219d969d740a25da79edbe9
Kenny Chirombo <chirombokenny@gmail.com>
2026-05-18 08:26:11 +0200

    add analytics to vercel site

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 );
 
